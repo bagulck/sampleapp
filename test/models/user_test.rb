d5299d28ac4df1ffd2876b2dda7ebd226ae31895
Bagulck <ckbagul@gmail.com>
2023-01-12 16:25:49 +0530

    finished modeling user

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,20 +2,20 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
   def setup
     @user = User.new(name: "Example User", email: "user@example.com",
-    password: "foobar", password_confirmation: "foobar" )
+                     password: "foobar", password_confirmation: "foobar")
   end
   test "should be valid" do
     assert @user.valid?
   end
   test "name should be present" do
-    @user.name = "    "
+    @user.name = "   "
     assert_not @user.valid?
   end
   test "email should be present" do
-    @user.email = "    "
+    @user.email = "   "
     assert_not @user.valid?
   end
-  test "Name should not be too long" do
+  test "name should not be too long" do
     @user.name = "a" * 51
     assert_not @user.valid?
   end
@@ -39,11 +39,17 @@ foo@bar_baz.com foo@bar+baz.com]
       assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
     end
   end
-  test "email address should be unique" do
+  test "email addresses should be unique" do
     duplicate_user = @user.dup
     duplicate_user.email = @user.email.upcase
     @user.save
     assert_not duplicate_user.valid?
+  end
+  test "email addresses should be saved as lower-case" do
+    mixed_case_email = "Foo@ExAMPle.CoM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
   end
   test "password should be present (nonblank)" do
     @user.password = @user.password_confirmation = " " * 6
@@ -53,4 +59,4 @@ foo@bar_baz.com foo@bar+baz.com]
     @user.password = @user.password_confirmation = "a" * 5
     assert_not @user.valid?
   end
-  end
+end
