52353f687e8eba0f13f901325cabcc8595c7a43b
Bagulck <ckbagul@gmail.com>
2023-01-24 11:11:42 +0530

    completed advanced login

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,7 +1,8 @@
 require "test_helper"
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
-    test "Log in with invalid information" do
+
+  test "Log in with invalid information" do
       get login_path
       assert_template 'sessions/new'
       post login_path, params: { session: { email: "", password: "" } }
@@ -9,7 +10,30 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
       assert_not flash.empty?
       get root_path
       assert flash.empty?
-    end
+  end
   end
   #blank break line
-
+=begin
+test "login with valid information followed by logout" do
+  get login_path
+  post login_path, params: { session: { email: @user.email,
+                                        password: 'password' } }
+  assert is_logged_in?
+  assert_redirected_to @user
+  follow_redirect!
+  assert_template 'users/show'
+  assert_select "a[href=?]", login_path, count: 0
+  assert_select "a[href=?]", logout_path
+  assert_select "a[href=?]", user_path(@user)
+  delete logout_path
+  assert_not is_logged_in?
+  assert_redirected_to root_url
+  # Simulate a user clicking logout in a second window.
+  delete logout_path
+  follow_redirect!
+  assert_select "a[href=?]", login_path
+  assert_select "a[href=?]", logout_path, count: 0
+  assert_select "a[href=?]", user_path(@user), count: 0
+  end
+end
+=end
